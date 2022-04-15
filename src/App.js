c6fc0124e5cfc97681e9cb22b9a373e6d6567732
solohad1n <fox663399@gmail.com>
2022-04-15 17:15:32 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className='App'>Apppppawdawdpp</div>;
+  return <div className='App'>Apppppawdawd hgjhbuhihgipp</div>;
 }
 
 export default App;
